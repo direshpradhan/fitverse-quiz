a7e92792b5c71ed0e9523d0e65bee5442928aeb6
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-09-07 13:08:20 +0530

    fix: quiz data not loading in first load

--- src/context/quiz/QuizContext.tsx
+++ src/context/quiz/QuizContext.tsx
@@ -8,6 +8,7 @@ import {
 } from "react";
 import { initialState, quizReducer } from "../../reducer/QuizReducer";
 import { API_URL } from "../../utils/constants";
+import { useAuth } from "../auth/AuthContext";
 import { QuizContextType } from "./QuizContext.types";
 
 const QuizContext = createContext<QuizContextType>({
@@ -17,23 +18,25 @@ const QuizContext = createContext<QuizContextType>({
 
 export const QuizProvider: FunctionComponent = ({ children }) => {
   const [state, dispatch] = useReducer(quizReducer, initialState);
+  const { token } = useAuth();
   console.log("entered quiz context");
   useEffect(() => {
-    (async function () {
-      console.log("entered quiz context useeffect....");
-      try {
-        const response = await axios.get(`${API_URL}/quiz`);
-        if (response.status === 200) {
-          dispatch({
-            type: "INITIALIZE_ALL_QUIZZES",
-            payload: response.data.quizzes,
-          });
+    token &&
+      (async function () {
+        console.log("entered quiz context useeffect....");
+        try {
+          const response = await axios.get(`${API_URL}/quiz`);
+          if (response.status === 200) {
+            dispatch({
+              type: "INITIALIZE_ALL_QUIZZES",
+              payload: response.data.quizzes,
+            });
+          }
+        } catch (error) {
+          console.log("Error while getting data from backend...", error);
         }
-      } catch (error) {
-        console.log("Error while getting data from backend...", error);
-      }
-    })();
-  }, []);
+      })();
+  }, [token]);
 
   return (
     <QuizContext.Provider
